c0f4f07e3ebc525ea15920e86c10b2f4afdeb892
Aeden-Agboola <131190544+Aeden-Agboola@users.noreply.github.com>
2023-08-15 21:39:24 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ img_y = 100;
 function add() {
 	img_imgTag = new Image(); //defining a variable with a new image
 	img_imgTag.onload = uploadimg; // setting a function, onloading this variable
-	img_imgTag.src = img_image;   // load image
+	img_imgTag.src = img_image; // load image
 }
 
 function uploadimg() {
@@ -24,39 +24,42 @@ function uploadimg() {
 //Write a code to grab the key-pressed event
 window.addEventListener("keydown", my_keydown);
 
-function my_keydown(e)
-{
+function my_keydown(e) {
 	keyPressed = e.keyCode;
 	console.log(keyPressed);
-	
-		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
-		
-	else{
+
+	if ((keyPressed >= 97 && keyPressed <= 122) || (keyPressed >= 65 && keyPressed <= 90)) {
 		alphabetkey();
-		document.getElementById("d1").innerHTML="You pressed alphabet key";
-		console.log
+		document.getElementById("d1").innerHTML = "You pressed alphabet key";
+
+	} else if (keyPressed >= 48 && keyPressed <= 57) {
+		numberkey();
+		document.getElementById("d1").innerHTML = "You pressed alphabet key";
+	} else {
+
 	}
 }
 
-function alphabetkey()
-{
-	img_image="Alpkey.png";
+function alphabetkey() {
+	img_image = "Alpkey.png";
+	add()
+}
 
+function numberkey() {
+	img_image = "numkey.png"
+	add()
 }
-function numberkey()
-{
-	
+
+function arrowkey() {
+	img_image = "Arrkey.png"
+	add()
 }
-function arrowkey()
-{
+
+function specialkey() {
+	img_image = "spkey.png"
 }
-function specialkey()
-{
-	
-}
-function otherkey()
-{
-	img_image="otherkey.png";
+
+function otherkey() {
+	img_image = "otherkey.png";
 	add();
 }
-	
